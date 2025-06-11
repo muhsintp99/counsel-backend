5213e773c58d339efc1d15b57e3c6e866bf0417d
muhsin <muhsintp.develop@gmail.com>
2025-06-11 16:26:40 +0530

    check

--- server.js
+++ server.js
@@ -111,6 +111,7 @@ app.set('trust proxy', 1);
 // ✅ Safe CORS: support both local & production
 const allowedOrigins = [
   "http://localhost:4040",
+  "https://counsel-frontend-4mh3.vercel.app/",
   "http://127.0.0.1:5503",
 ];
 
